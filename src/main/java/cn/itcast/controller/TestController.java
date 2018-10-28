21d1303ad5be79eca6e20de6b826efccc9f0fa61
lvrongzhu <18055457981@163.com>
2018-10-28 16:59:11 +0800

    还原后的提交

--- src/main/java/cn/itcast/controller/TestController.java
+++ src/main/java/cn/itcast/controller/TestController.java
@@ -2,6 +2,7 @@ package cn.itcast.controller;
 
 public class TestController {
     public static void main(String[] args) {
-        System.out.println("测试idea中提交后的还原");
+
+        System.out.println("还原后的提交");
     }
 }
